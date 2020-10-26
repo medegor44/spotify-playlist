36ee2030e2f75d15da2c03818a06e48c64a11542
medegor44 <medegor44@gmail.com>
2020-10-26 10:54:41 +0300

    Fix after review

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { SpotifyLogin } from "./SpotifyLogin";
-import { SpotifyAuthRedirect } from "./SpotifyAuthRedirect";
 import { SpotifyPlaylists } from "./SpotifyPlaylists";
 
 export const App = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/:access_token(access_token=.*)">
-          <SpotifyAuthRedirect />
-        </Route>
         <Route path="/">
           <SpotifyLogin />
           <SpotifyPlaylists />
--- src/SpotifyLoginButton.jsx
+++ src/SpotifyLoginButton.jsx
@@ -4,7 +4,7 @@ import { generateAuthUrl } from "./utils/spotify";
 export const SpotifyLoginButton = () => {
   const initiateSpotifyLogin = () => {
     const url = window.location.href.split("?")[0];
-    window.location.href = generateAuthUrl(`${url}auth`);
+    window.location.href = generateAuthUrl(url);
   };
 
   return <button onClick={initiateSpotifyLogin}>Log in to spotify</button>;
