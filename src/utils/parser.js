ba0f34abb2e5ab872965ebe960925bf4d7e65a7b
medegor44 <medegor44@gmail.com>
2020-11-06 09:45:49 +0300

    Fix parsing error, when whitespace strings were treated as tracks

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -1,15 +1,18 @@
 const parseArtistsTracks = (text) => {
-  return text.split("\n").map((artistTrackStr) => {
-    const splitArtistTrack = artistTrackStr.split(" - ");
+  return text
+    .split("\n")
+    .filter((artistTrackStr) => artistTrackStr.trim().length)
+    .map((artistTrackStr) => {
+      const splitArtistTrack = artistTrackStr.split(" - ");
 
-    if (splitArtistTrack.length < 2)
-      return { artist: splitArtistTrack[0].trim(), track: "" };
+      if (splitArtistTrack.length < 2)
+        return { artist: splitArtistTrack[0].trim(), track: "" };
 
-    return {
-      artist: splitArtistTrack[0].trim(),
-      track: splitArtistTrack[1].trim(),
-    };
-  });
+      return {
+        artist: splitArtistTrack[0].trim(),
+        track: splitArtistTrack[1].trim(),
+      };
+    });
 };
 
 export default parseArtistsTracks;
