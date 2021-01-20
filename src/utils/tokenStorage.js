d22f87884f846860ad67f9c6ac982e71f9f6172b
medegor44 <medegor44@gmail.com>
2021-01-20 17:11:15 +0300

    bugix refreshig expired token

--- src/utils/tokenStorage.js
+++ src/utils/tokenStorage.js
@@ -1,3 +1,12 @@
-export const setToken = (token) => sessionStorage.setItem("token", token);
+export const setTokenInfo = (token, expirationDate) => {
+  sessionStorage.setItem("token", token);
+  sessionStorage.setItem("expirationDate", expirationDate);
+};
 
 export const getToken = () => sessionStorage.getItem("token");
+export const getTokenExpirationDate = () =>
+  sessionStorage.getItem("expirationDate");
+export const clearTokenInfo = () => {
+  sessionStorage.removeItem("token");
+  sessionStorage.removeItem("expirationDate");
+};
